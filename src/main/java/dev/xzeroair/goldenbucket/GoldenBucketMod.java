e675f9fd8fc42e5f7b65210113704bc8d29ef772
XzeroAir <XzeroAir@gmail.com>
2026-04-10 15:44:18 -0400

    1.18.2 initial commit

---
+++ src/main/java/dev/xzeroair/goldenbucket/GoldenBucketMod.java
@@ -0,0 +1,31 @@
+package dev.xzeroair.goldenbucket;
+
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.common.ForgeMod;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+@Mod(GoldenBucketMod.MODID)
+public class GoldenBucketMod {
+    public static final String MODID = "goldenbucket";
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
+    public static final RegistryObject<Item> GOLDEN_BUCKET = ITEMS.register("golden_bucket", GoldenBucketItem::new);
+
+    public GoldenBucketMod() {
+        ForgeMod.enableMilkFluid();
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        ITEMS.register(modEventBus);
+        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, GoldenBucketConfig.SPEC);
+    }
+
+    static Item.Properties itemProperties() {
+        return new Item.Properties().stacksTo(1).tab(CreativeModeTab.TAB_TOOLS);
+    }
+}
